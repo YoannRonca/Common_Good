6513e3368dd1d50d4448b831ab6e3b94bfb24005
Livia Lortscher <livialortscher@hotmail.com>
2019-02-28 15:57:31 +0100

    added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_144758) do
+ActiveRecord::Schema.define(version: 2019_02_28_143703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_144758) do
     t.string "title"
     t.string "description"
     t.string "photo"
+    t.string "home_category"
     t.index ["organization_id"], name: "index_missions_on_organization_id"
   end
 
