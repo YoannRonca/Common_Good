25f31eb9c66815041064ec0489e0d24c6050c8a9
yoann roncajolo <yoann.roncajolo@gmail.com>
2019-02-28 15:45:26 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
   resource :users, only: :show
-  resources :profiles, only: :index
+  resources :profiles, only: :show
 end
