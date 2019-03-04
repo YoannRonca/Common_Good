3e9ff1f1086dc1caee7957f55811b91b3415616c
Livia Lortscher <livialortscher@hotmail.com>
2019-03-04 14:26:44 +0100

    login changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_083625) do
+ActiveRecord::Schema.define(version: 2019_03_04_130045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 2019_03_04_083625) do
     t.integer "phone_number"
     t.text "description"
     t.boolean "admin"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
