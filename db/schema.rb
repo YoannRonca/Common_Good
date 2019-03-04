59e9134959abc8895ae111006f99118a31d6f809
Livia Lortscher <livialortscher@hotmail.com>
2019-03-04 17:39:11 +0100

    dashboard 2/5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_130045) do
+ActiveRecord::Schema.define(version: 2019_03_04_142914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_03_04_130045) do
     t.bigint "mission_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["mission_id"], name: "index_mission_users_on_mission_id"
     t.index ["user_id"], name: "index_mission_users_on_user_id"
   end
