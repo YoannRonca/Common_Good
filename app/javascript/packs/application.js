e9b108e68526071b6dfd92d2f7f982c94d0039e2
Bruno Flores <brunogfloresvela@gmail.com>
2019-03-04 11:19:12 +0100

    added map and slight changes to show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,3 +9,9 @@ initSweetalert('#sweet-alert-demo', {
   text: "Your application has been sent!",
   icon: "success"
 });
+
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_114103) do
+ActiveRecord::Schema.define(version: 2019_03_04_083625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2019_03_01_114103) do
     t.string "description"
     t.string "photo"
     t.string "home_category"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["organization_id"], name: "index_missions_on_organization_id"
   end
 
